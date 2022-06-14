8890b3d79d4578e5113cd75cb6e61938a4091956
l0st-idi0t <carlnod121@gmail.com>
2022-06-13 22:53:27 -0500

    putting on hold for now

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
-// // Scroll to specific values
-// // scrollTo is the same
-// window.scroll({
-//   top: 2500, 
-//   left: 0, 
-//   behavior: 'smooth'
-// });
+const body = document.body;
 
-// Scroll certain amounts from current position 
-window.scrollBy({ 
-  top: 100, // could be negative value
-  left: 0, 
-  behavior: 'smooth' 
-});
+let sx = 0, // For scroll positions
+    sy = 0;
+
+
+// Bind a scroll function
+window.addEventListener('scroll', easeScroll);
+
+
+function easeScroll() {
+  sx = window.pageXOffset;
+  sy = window.pageYOffset;
+}
+
+
+window.requestAnimationFrame(render);
+
+function render(){
+  //We calculate our container position by linear interpolation method
+  sx = li(sx,sx,0.07);
+  sy = li(sy,sy,0.07);
+  
+  sx = Math.floor(sx * 100) / 100;
+  sy = Math.floor(sy * 100) / 100;
+  
+  
+  // main.style.transform = `translate3d(-${dx}px, -${dy}px, 0px)`;
+ 
+  
+  
+  window.requestAnimationFrame(render);
+}
+
+function li(a, b, n) {
+  return (1 - n) * a + n * b;
+}
